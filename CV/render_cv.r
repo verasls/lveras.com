5c4f0fe3d596f8a4d2e05fa18a23b6f46aba91f2
Lucas <lucasdsveras@gmail.com>
2021-09-23 16:34:27 -0300

    Format code

--- CV/render_cv.r
+++ CV/render_cv.r
@@ -6,16 +6,22 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
-rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = FALSE),
-                  output_file = "cv.html")
+rmarkdown::render(
+  "cv.rmd",
+  params = list(pdf_mode = FALSE),
+  output_file = "lv_cv.html"
+)
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = TRUE),
-                  output_file = tmp_html_cv_loc)
+rmarkdown::render(
+  "cv.rmd",
+  params = list(pdf_mode = TRUE),
+  output_file = tmp_html_cv_loc
+)
 
 # Convert to PDF using Pagedown
-pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+pagedown::chrome_print(
+  input = tmp_html_cv_loc,
+  output = "lv_cv.pdf"
+)
