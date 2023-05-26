8758a00925f58af7842302d46e0ffe8967be3ac3
Lucas <lucasdsveras@gmail.com>
2023-05-26 15:13:20 +0100

    Delete file

--- src/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const yearEl = document
-  .querySelector("header-component")
-  .querySelector("header")
-  .querySelector(".year");
-
-yearEl.textContent = new Date().getFullYear();
