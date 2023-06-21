82d738bddf79aaeda47e4d095e5efa95622046f5
Lucas <lucasdsveras@gmail.com>
2023-06-21 17:41:03 +0100

    Refactor Footer component and add back the copyright text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,51 @@ import "./Footer.css";
 
 export function Footer() {
   return (
-    <footer className="social-media">
-      <a
-        className="social-media__link"
-        href="https://github.com/verasls"
-        aria-label="Lucas Veras Github profile"
-      >
-        <IonIcon className="social-media__icon" icon={logoGithub} />
-      </a>
-      <a
-        className="social-media__link"
-        href="https://www.linkedin.com/in/lucas-veras-17393115a/"
-        aria-label="Lucas Veras Linkedin profile"
-      >
-        <IonIcon className="social-media__icon" icon={logoLinkedin} />
-      </a>
-      <a
-        className="social-media__link"
-        href="https://twitter.com/lucasdsveras"
-        aria-label="Lucas Veras Twitter profile"
-      >
-        <IonIcon className="social-media__icon" icon={logoTwitter} />
-      </a>
+    <footer className="footer">
+      <SocialMedia />
+      <Copyright />
     </footer>
+  );
+}
+
+function SocialMedia() {
+  return (
+    <ul className="social-media">
+      <li>
+        <a
+          className="social-media__link"
+          href="https://github.com/verasls"
+          aria-label="Lucas Veras Github profile"
+        >
+          <IonIcon className="social-media__icon" icon={logoGithub} />
+        </a>
+      </li>
+      <li>
+        <a
+          className="social-media__link"
+          href="https://www.linkedin.com/in/lucas-veras-17393115a/"
+          aria-label="Lucas Veras Linkedin profile"
+        >
+          <IonIcon className="social-media__icon" icon={logoLinkedin} />
+        </a>
+      </li>
+      <li>
+        <a
+          className="social-media__link"
+          href="https://twitter.com/lucasdsveras"
+          aria-label="Lucas Veras Twitter profile"
+        >
+          <IonIcon className="social-media__icon" icon={logoTwitter} />
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+function Copyright() {
+  return (
+    <p className="copyright">
+      Copyright &copy; {new Date().getFullYear()} by Lucas Veras
+    </p>
   );
 }
