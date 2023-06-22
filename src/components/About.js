332372924f9e79307db9078f390b0716bdf236f9
Lucas <lucasdsveras@gmail.com>
2023-06-22 17:54:27 +0100

    Refactor About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,74 @@
+import { IonIcon } from "@ionic/react";
+import {
+  barbellOutline,
+  barChartOutline,
+  codeSlashOutline,
+} from "ionicons/icons";
 import { NavHashLink } from "react-router-hash-link";
 
 import "./About.css";
 
 export function About() {
+  const aboutData = [
+    {
+      title: "Sport scientist",
+      icon: barbellOutline,
+      text: "PhD researcher interested in how to measure physical activity using wearable devices data",
+    },
+    {
+      title: "Data analyst",
+      icon: barChartOutline,
+      text: "Specialist in statistical learning, data visualization, time-series and biological signals",
+    },
+    {
+      title: "Developer",
+      icon: codeSlashOutline,
+      text: "Front-end mobile and web developer, creating digital solutions to everyday problems",
+    },
+  ];
+
   return (
     <section className="section-about" id="about">
       <div className="about">
-        <p className="text">Hi,</p>
+        <span className="subheading">About me</span>
+        <h2>Hello there!</h2>
         <p className="text">
-          I'm a sport scientist who began working in data science during my
-          master's degree. Through my PhD research, I gained extensive
-          experience in data analysis, modeling, and visualization, which has
-          enabled me to extract insights from complex data sets. As a frontend
-          web developer, I'm passionate about creating intuitive, engaging and
-          beautiful user experiences on the web. With my diverse skillset, I'm
-          dedicated to developing innovative solutions that bridge the gap
-          between sports science, data science, and web development. Thank you
-          for visiting my website, and please feel free to{" "}
+          These are my main areas of work. Feel free to{" "}
           <NavHashLink
             className="link"
             to="/#contact"
             smooth={true}
             duration={500}
           >
-            contact me
+            reach out
           </NavHashLink>{" "}
-          if you have any questions or if you need my help with your project.
+          if you have any questions or if you need my help.
         </p>
+
+        <AboutList data={aboutData} />
       </div>
     </section>
+  );
+}
+
+function AboutList({ data }) {
+  return (
+    <ul className="about__list">
+      {data.map((data) => (
+        <AboutItem key={data.title} data={data} />
+      ))}
+    </ul>
+  );
+}
+
+function AboutItem({ data }) {
+  return (
+    <li className="about__item">
+      <div className="about__header">
+        <IonIcon className="about__icon" icon={data.icon} />
+        <span className="about__title">{data.title}</span>
+      </div>
+      <span className="about__txt">{data.text}</span>
+    </li>
   );
 }
