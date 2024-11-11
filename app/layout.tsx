ad568f524bee5eb4b7e03ccca6f193b9d4df80c0
Lucas <lucasdsveras@gmail.com>
2024-11-10 22:46:54 -0300

    Add a basic main content

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Lucas Veras</h1>;
+  return (
+    <div className="mx-auto flex min-h-screen max-w-3xl items-center justify-start">
+      <div>
+        <h1 className="mb-5 text-5xl font-bold">Lucas Veras</h1>
+        <p className="text-lg">PhD candidate in Physical Activity and Health</p>
+        <p className="text-lg">Faculty of Sport, University of Porto</p>
+      </div>
+    </div>
+  );
 }
