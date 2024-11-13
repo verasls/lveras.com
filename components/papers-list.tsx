914277244413b597470643e1b976fabc38f581aa
Lucas <lucasdsveras@gmail.com>
2024-11-13 17:23:26 -0300

    Add an animation to the papers list

--- components/papers-list.tsx
+++ components/papers-list.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { badgeVariants } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Paper } from "@/app/papers/papers-data";
+
+type Position = {
+  top: number;
+  height: number;
+  opacity: number;
+};
 
 type PapersListProps = {
   papers: Array<Paper>;
@@ -45,72 +54,142 @@ export default function PapersList({ papers }: PapersListProps) {
   return (
     <div className="mt-14 flex flex-col gap-6">
       {sortedYears.map((year) => (
-        <div key={year}>
-          <h3 className="mb-2 text-lg font-bold">{year}</h3>
-
-          <ul className="flex flex-col gap-4">
-            {groupedPapers[year].map((paper) => (
-              <li
-                key={paper.id}
-                className="flex flex-col gap-2 rounded-md p-4 hover:bg-accent"
-              >
-                <h4 className="text-lg font-semibold">{paper.title}</h4>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: formatAuthors(paper.authors),
-                  }}
-                  className="text-sm"
-                />
-                <p className="text-sm italic">{paper.journal}</p>
-
-                <div className="mt-3 flex h-5 items-center gap-2">
-                  {paper.doi ? (
-                    <Link
-                      href={`https://doi.org/${paper.doi}`}
-                      className={cn(
-                        badgeVariants({ variant: "outline" }),
-                        "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
-                      )}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      doi
-                    </Link>
-                  ) : null}
-
-                  {paper.pmid ? (
-                    <Link
-                      href={`https://pubmed.ncbi.nlm.nih.gov/${paper.pmid}`}
-                      className={cn(
-                        badgeVariants({ variant: "outline" }),
-                        "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
-                      )}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      pubmed
-                    </Link>
-                  ) : null}
-
-                  {paper.code ? (
-                    <Link
-                      href={`https://github.com/verasls/${paper.code}`}
-                      className={cn(
-                        badgeVariants({ variant: "outline" }),
-                        "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
-                      )}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      code
-                    </Link>
-                  ) : null}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <PapersPerYear key={year} year={year} papers={groupedPapers[year]} />
       ))}
     </div>
+  );
+}
+
+type PapersPerYearProps = {
+  year: number;
+  papers: Array<Paper>;
+};
+
+function PapersPerYear({ year, papers }: PapersPerYearProps) {
+  const [position, setPosition] = useState<Position>({
+    top: 0,
+    height: 0,
+    opacity: 0,
+  });
+
+  return (
+    <div>
+      <h3 className="mb-2 text-lg font-bold">{year}</h3>
+
+      <ul
+        onMouseLeave={() => {
+          setPosition((previousPosition) => ({
+            ...previousPosition,
+            opacity: 0,
+          }));
+        }}
+        className="relative flex flex-col gap-4"
+      >
+        {papers.map((paper) => (
+          <PapersListItem
+            key={paper.id}
+            paper={paper}
+            setPosition={setPosition}
+          />
+        ))}
+
+        <Cursor position={position} />
+      </ul>
+    </div>
+  );
+}
+
+type PapersListItemProps = {
+  paper: Paper;
+  setPosition: React.Dispatch<React.SetStateAction<Position>>;
+};
+
+function PapersListItem({ paper, setPosition }: PapersListItemProps) {
+  const ref = useRef<HTMLLIElement>(null);
+
+  return (
+    <li
+      key={paper.id}
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref.current) return;
+
+        const { height, top } = ref.current.getBoundingClientRect();
+        const offsetTop = ref.current.offsetTop;
+
+        setPosition({
+          top: offsetTop,
+          height: height,
+          opacity: 1,
+        });
+      }}
+      className="relative z-10 flex flex-col gap-2 rounded-md p-4"
+    >
+      <h4 className="text-lg font-semibold">{paper.title}</h4>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: formatAuthors(paper.authors),
+        }}
+        className="text-sm"
+      />
+      <p className="text-sm italic">{paper.journal}</p>
+
+      <div className="mt-3 flex h-5 items-center gap-2">
+        {paper.doi ? (
+          <Link
+            href={`https://doi.org/${paper.doi}`}
+            className={cn(
+              badgeVariants({ variant: "outline" }),
+              "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
+            )}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            doi
+          </Link>
+        ) : null}
+
+        {paper.pmid ? (
+          <Link
+            href={`https://pubmed.ncbi.nlm.nih.gov/${paper.pmid}`}
+            className={cn(
+              badgeVariants({ variant: "outline" }),
+              "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
+            )}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            pubmed
+          </Link>
+        ) : null}
+
+        {paper.code ? (
+          <Link
+            href={`https://github.com/verasls/${paper.code}`}
+            className={cn(
+              badgeVariants({ variant: "outline" }),
+              "border-foreground text-xs font-light uppercase tracking-wide hover:bg-background/40"
+            )}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            code
+          </Link>
+        ) : null}
+      </div>
+    </li>
+  );
+}
+
+type CursorProps = {
+  position: Position;
+};
+
+function Cursor({ position }: CursorProps) {
+  return (
+    <motion.li
+      animate={{ ...position, left: "0", transform: "translateX(0)" }}
+      className="absolute z-0 w-full rounded-md bg-accent"
+    />
   );
 }
