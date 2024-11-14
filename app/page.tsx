7d9c16e907b4812f3fee66e15b1b04df64166d35
Lucas <lucasdsveras@gmail.com>
2024-11-13 22:19:54 -0300

    Detect the device type on server to prevent hydration mismatch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import CommandMenuHomeText from "@/components/command-menu-home-text";
+import { headers } from "next/headers";
+import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default async function Page() {
+  const headerList = await headers();
+  const userAgent = headerList.get("user-agent") || "";
+  const isMac = /Macintosh/.test(userAgent);
+  const isMobile =
+    /iPad|iPhone|iPod/.test(userAgent) || /android/i.test(userAgent);
+  const deviceType = isMobile ? "mobile" : isMac ? "mac" : "other";
+
   return (
     <div className="mx-auto flex h-[calc(100vh-80px-64px)] max-w-3xl flex-1 items-center justify-start px-6">
       <div>
@@ -12,7 +20,35 @@ export default function Page() {
         </p>
         <p className="mb-8">Faculty of Sport, University of Porto</p>
 
-        <CommandMenuHomeText />
+        {deviceType === "mobile" ? (
+          <Button variant="ghost" className="-ml-4">
+            Tap to start
+          </Button>
+        ) : (
+          <p className="text-sm font-medium">
+            Press{" "}
+            {deviceType === "mac" ? (
+              <>
+                <kbd className="pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
+                  ⌘
+                </kbd>{" "}
+                <kbd className="pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
+                  K
+                </kbd>
+              </>
+            ) : (
+              <>
+                <kbd className="pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
+                  ctrl
+                </kbd>{" "}
+                <kbd className="pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
+                  K
+                </kbd>
+              </>
+            )}{" "}
+            to start
+          </p>
+        )}
       </div>
     </div>
   );
