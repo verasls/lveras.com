d6048fe1fac413b6c2b54c7fbfd3a9df7cadf8c3
Lucas <lucasdsveras@gmail.com>
2024-11-18 17:18:00 -0300

    Add the about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,28 @@
 export default function Page() {
-  return <h1>About</h1>;
+  return (
+    <div className="mx-auto max-w-3xl pt-6">
+      <h1 className="text-gradient mb-4 text-4xl font-bold tracking-tight">
+        About
+      </h1>
+      <p className="my-4 leading-7">
+        Hello! I&apos;m Lucas Veras, a sports scientist nearing the completion
+        of my PhD in Physical Activity and Health at the Faculty of Sport,
+        University of Porto. My academic journey began with a Bachelor&apos;s
+        degree in Physical Education from the Faculty of Alagoas, Brazil,
+        followed by a Master&apos;s degree at the same institution where I am
+        now completing my doctoral studies.
+      </p>
+      <p className="my-4 leading-7">
+        Throughout my Master&apos;s and PhD, my research has focused on
+        statistical modeling and data analysis from wearable devices, especially
+        accelerometers. My main work involves the development of methods for
+        estimating mechanical loading variables using raw accelerometer data.
+      </p>
+      <p className="my-4 leading-7">
+        I&apos;m also a husband and a proud father to a little boy. Beyond my
+        professional life, I have a deep love for running, which has taken me
+        across the finish lines of two marathons and three half-marathons.
+      </p>
+    </div>
+  );
 }
